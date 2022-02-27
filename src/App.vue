a84dea10072e676bd4865fcbe9dc153825dce650
fairlyxu <feilibaby@163.com>
2022-02-27 22:22:34 +0800

    feature:sol cli with vue3

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+body,
+p {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  -moz-background-size: 100% 100%;
+  background-size: 100% 100%;
+  background-color: rgba(0, 0, 0, 1);
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,223 @@
+<template>
+  <div class="hello">
+    <div class="header">
+      <span class="title">ReadOn</span>
+      <span class="overview">overview</span>
+    </div>
+    <div class="sec sec1">
+      <div class="content">
+        <p class="t2 font-point"
+           style="color: rgba(255, 52, 0, 1);">ReadON</p>
+        <p class="t2">Addicted to Reading</p>
+        <p class="t3">ReadOn is an amazing "Read to Earn" program that combines SOCIAL-FI AND GAME-FI.Get back into the habit of reading and get addicted, just like playing a game.</p>
+        <el-row class="mb-4"
+                style="margin-top: 3em;">
+          <el-button round
+                     class="buttom"
+                     style="color:white;background: #5961ec;">Join Discord</el-button>
+          <el-button round>White Paper</el-button>
+        </el-row>
+      </div>
+    </div>
+
+    <div class="sec sec2">
+      <div class="content">
+        <p class="t2">Mint Your <span class="font-point"
+                style="color: rgba(255, 52, 0, 1);"> Nucleus </span></p>
+        <p class="t2">To Earn $READ</p>
+        <p class="t3">Choose a nucleus you love as a NFT and start reading the game.Read, upgrade, and get achievements to earn extra tokens.
+        </p>
+      </div>
+    </div>
+
+    <div class="sec sec3">
+      <div class="content">
+        <p class="t2">Unleashing Your Power </p>
+        <p class="t2">Of <span class="font-point"
+                style="color:rgba(255,179,0,1);"> Fission </span></p>
+        <p class="t3">Spread the content you like and let your friends feel your influence.Find people with the same hobbies and
+          unlock your favorite props together to gain whitelist rights.
+        </p>
+
+      </div>
+    </div>
+
+    <div class="sec sec4">
+      <div class="content">
+        <p class="t2">Feel the White Heat of </p>
+        <p class="t2 font-point"
+           style="color:rgba(0,177,255,1);">Web3</p>
+        <p class="t3">Wow, ReadOn is a web3 project, it will take you into web3, don't miss it.Start by following our community and you will be involved in the governance of an exciting program!
+        </p>
+
+      </div>
+    </div>
+
+    <div class="footer">
+      <div class="foot-main">
+        <p>To bring the old and classic "reading" back to our life and to distribute high-quality, genuine and valuable thoughts to the world.</p>
+        <div class="box">
+          <img class="b-title"
+               src="../assets/imgs/twitter.png">
+          <img class="b-title"
+               src="../assets/imgs/discord.png">
+          <img class="b-title"
+               src="../assets/imgs/telegram.png">
+          <img class="b-title"
+               src="../assets/imgs/medium.png">
+        </div>
+
+        <div class="line_02"></div>
+        <p>Copyright 2022. All Right Reserved</p>
+      </div>
+      <img class="foot-shadow"
+           src="../assets/imgs/shadow.png">
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* header*/
+.header {
+  background-color: black;
+  color: white;
+  height: 2.5em;
+  line-height: 2.5em;
+  border-bottom: 1px solid white;
+}
+.title {
+  text-align: left;
+  padding-left: 10%;
+  font-weight: bold;
+  font-size: 1.5em;
+}
+.overview {
+  float: right;
+  text-align: right;
+  padding-right: 10%;
+  font-size: 1em;
+}
+
+/*section*/
+.buttom {
+  border: none;
+  font-weight: bold;
+}
+.font-point {
+  font-weight: bolder;
+  font-size: 1.1em;
+  white-space: nowrap;
+}
+
+.t2 {
+  font-weight: bold;
+  font-size: 3.5em;
+  color: white;
+  margin-top: 0.5em;
+}
+.t3 {
+  font-style: normal;
+  font-weight: lighter;
+  font-size: 1.5em;
+  color: rgba(245, 245, 245, 1);
+  letter-spacing: 1px;
+  margin-top: 0.5em;
+}
+.sec {
+  padding-top: 7em;
+  padding-bottom: 7em;
+  background: grey;
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  -moz-background-size: 100% 100%;
+}
+.sec1 {
+  background-image: url(../assets/imgs/s1.png);
+}
+.sec2 {
+  background-image: url(../assets/imgs/s2.png);
+}
+.sec3 {
+  background-image: url(../assets/imgs/s3.png);
+}
+.sec4 {
+  background-image: url(../assets/imgs/s4.png);
+}
+
+.contect {
+  margin: 0 auto;
+}
+
+.sec:nth-child(even) {
+  text-align: left;
+  padding-left: 10%;
+  padding-right: 48%;
+}
+
+.sec:nth-child(odd) {
+  text-align: right;
+  padding-left: 48%;
+  padding-right: 10%;
+}
+
+/*footer */
+.footer {
+  margin: 0 auto;
+  width: 80%;
+  padding: 5em;
+}
+.foot-main > p {
+  margin: 0 auto;
+  text-align: center;
+  width: 75%;
+  font-size: 1.2em;
+  padding: 1em;
+}
+
+.foot-main {
+  background-color: white;
+  border: 1px solid;
+  border-radius: 20px;
+}
+.foot-shadow {
+  width: 100%;
+  margin-top: -8px;
+}
+.foot-shadow1 {
+  margin-top: 0em;
+  border-top: 50px solid #5a5756;
+  border-left: 50px solid transparent;
+  border-right: 50px solid transparent;
+  background-image: linear-gradient(#e66465, #9198e5);
+}
+
+.box {
+  display: flex;
+  margin: 1em auto;
+  width: 40%;
+}
+
+.b-title {
+  height: 2em;
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  margin-right: 3em;
+}
+
+.line_02 {
+  width: 70%;
+  margin: 0 auto;
+  height: 1px;
+  border-top: 1px solid black;
+  text-align: center;
+}
+</style>
